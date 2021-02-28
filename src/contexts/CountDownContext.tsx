90d1e28d090ada95389fa4e1eb78a1bf477b169f
Diego Patriota <diego.patriota@saqueepague.com.br>
2021-02-28 15:03:08 -0300

    inclusion of data in cookies, creation of modal component to level up.

--- src/contexts/CountDownContext.tsx
+++ src/contexts/CountDownContext.tsx
@@ -21,7 +21,7 @@ let countDownTimeout: NodeJS.Timeout;
 export function CountDownProvider ({ children }: countDownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext);
-    const [time, setTime] = useState(0.1 * 60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { CountDownProvider } from '../contexts/CountDownContext';
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <CountDownProvider>
         <Component {...pageProps} />
-      </CountDownProvider>
-    </ChallengesProvider>
     )
 }
 
